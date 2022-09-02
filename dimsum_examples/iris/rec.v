cdec064d9c6b8650bbaf6f06bd82a976ab20f2f0
Michael Sammler <noreply@sammler.me>
2022-09-02 14:05:39 +0200

    fix remaining admitteds

--- dimsum_examples/iris/rec.v
+++ dimsum_examples/iris/rec.v
@@ -8,6 +8,9 @@ Set Default Proof Using "Type".
 
 Local Open Scope Z_scope.
 
+  Lemma loc_eq (l1 l2 : loc) :
+    l1 = l2 ↔ l1.1 = l2.1 ∧ l1.2 = l2.2.
+  Proof. destruct l1, l2; naive_solver. Qed.
 
 Section sim.
   Context {Σ : gFunctors} {EV : Type} `{!dimsumGS Σ}.
@@ -390,6 +393,63 @@ Section spec.
 End spec.
 
 
+Definition array (l : loc) (vs : list val) : gmap loc val :=
+  (kmap (λ i, l +ₗ i) (map_seqZ 0 vs)).
+
+Section array.
+  Lemma array_nil l :
+    array l [] = ∅.
+  Proof. rewrite /array/=. apply kmap_empty. Qed.
+  Lemma array_cons l v vs :
+    array l (v::vs) = <[l:=v]> $ array (l +ₗ 1) vs.
+  Proof.
+    rewrite /array/= kmap_insert offset_loc_0. f_equal.
+    apply map_eq => ?. apply option_eq => ?.
+    rewrite !lookup_kmap_Some. setoid_rewrite lookup_map_seqZ_Some.
+    split => -[i [? [? <-]]]; simplify_eq.
+    - eexists (Z.pred i). split!; [|lia|f_equal; lia].
+      unfold offset_loc. destruct l => /=. f_equal. lia.
+    - eexists (Z.succ i). split!; [|lia|f_equal; lia].
+      unfold offset_loc. destruct l => /=. f_equal. lia.
+  Qed.
+
+  Lemma array_app l vs1 vs2 :
+    array l (vs1 ++ vs2) = array l vs1 ∪ array (l +ₗ length vs1) vs2.
+  Proof.
+    elim: vs1 l. { move => ?/=. by rewrite array_nil offset_loc_0 left_id_L. }
+    move => v vs1 IH l /=. rewrite !array_cons -insert_union_l.
+    rewrite IH. do 3 f_equal. apply loc_eq. split!. lia.
+  Qed.
+
+  Lemma array_insert l1 l2 v vs :
+    l1.1 = l2.1 →
+    l2.2 ≤ l1.2 < l2.2 + length vs →
+    <[l1:=v]> $ array l2 vs = array l2 (<[Z.to_nat (l1.2 - l2.2):=v]>vs).
+  Proof.
+    move => ??. have {1} -> : l1 = l2 +ₗ Z.to_nat (l1.2 - l2.2).
+    { unfold offset_loc. destruct l1, l2; simplify_eq/=. f_equal. lia. }
+    rewrite /array/= map_seqZ_insert. 2: lia.
+    by rewrite kmap_insert.
+  Qed.
+
+  Lemma array_lookup_None l l' vs :
+    array l vs !! l' = None ↔ (l.1 = l'.1 → l'.2 < l.2 ∨ l.2 + length vs ≤ l'.2).
+  Proof.
+    rewrite /array.
+    rewrite lookup_kmap_None.
+    setoid_rewrite lookup_map_seqZ_None.
+    split.
+    - move => Hi Heq.
+      exploit (Hi (l'.2 - l.2)). { unfold offset_loc. destruct l, l'; simplify_eq/=. f_equal. lia. }
+      lia.
+    - move => Hi ??. simplify_eq/=. lia.
+  Qed.
+
+  Lemma array_lookup_is_Some l l' vs :
+    is_Some (array l vs !! l') ↔ l.1 = l'.1 ∧ l.2 ≤ l'.2 < l.2 + length vs.
+  Proof. rewrite -not_eq_None_Some array_lookup_None. naive_solver lia. Qed.
+End array.
+
 Definition fnsUR : cmra :=
   agreeR (gmapO string (leibnizO fndef)).
 
@@ -399,13 +459,13 @@ Definition to_fns : gmap string fndef → fnsUR :=
 
 Class recPreGS (Σ : gFunctors) := RecPreGS {
   rec_mapsto_ghost_map_preG :> ghost_mapG Σ loc val;
-  rec_alloc_ghost_map_preG :> ghost_mapG Σ loc Z;
+  rec_alloc_ghost_map_preG :> ghost_mapG Σ prov Z;
   rec_fn_preG :> inG Σ fnsUR;
 }.
 
 Class recGS (Σ : gFunctors) := RecGS {
   rec_mapsto_ghost_mapG :> ghost_mapG Σ loc val;
-  rec_alloc_ghost_mapG :> ghost_mapG Σ loc Z;
+  rec_alloc_ghost_mapG :> ghost_mapG Σ prov Z;
   rec_fnG :> inG Σ fnsUR;
   rec_mapsto_name : gname;
   rec_alloc_name : gname;
@@ -413,7 +473,7 @@ Class recGS (Σ : gFunctors) := RecGS {
 }.
 
 Definition recΣ : gFunctors :=
-  #[ ghost_mapΣ loc val; ghost_mapΣ loc Z; GFunctor fnsUR ].
+  #[ ghost_mapΣ loc val; ghost_mapΣ prov Z; GFunctor fnsUR ].
 
 Global Instance subG_recΣ Σ :
   subG recΣ Σ → recPreGS Σ.
@@ -434,7 +494,7 @@ Section definitions.
     ghost_map_auth rec_mapsto_name 1 h.
 
   Definition rec_alloc_def (l : loc) (sz : Z) : iProp Σ :=
-    ghost_map_elem rec_alloc_name l (DfracOwn 1) sz.
+    ⌜l.2 = 0⌝ ∗ ghost_map_elem rec_alloc_name l.1 (DfracOwn 1) sz.
   Local Definition rec_alloc_aux : seal (@rec_alloc_def).
   Proof. by eexists. Qed.
   Definition rec_alloc := rec_alloc_aux.(unseal).
@@ -442,10 +502,10 @@ Section definitions.
     @rec_alloc = @rec_alloc_def := rec_alloc_aux.(seal_eq).
 
   Definition rec_alloc_auth (h : gset loc) : iProp Σ :=
-    ∃ p,
-    ⌜∀ l sz, p !! l = Some sz → sz > 0⌝ ∗
-    ⌜∀ l sz, p !! l = Some sz → ∀ l', l'.1 = l.1 → l' ∈ h ↔ l.2 ≤ l'.2 < l.2 + sz⌝ ∗
-    ghost_map_auth rec_alloc_name 1 p.
+    ∃ m,
+    ⌜∀ p sz, m !! p = Some sz → sz > 0⌝ ∗
+    ⌜∀ p sz, m !! p = Some sz → ∀ l', l'.1 = p → l' ∈ h ↔ 0 ≤ l'.2 < sz⌝ ∗
+    ghost_map_auth rec_alloc_name 1 m.
 
   Definition rec_fn_auth (fns : gmap string fndef) : iProp Σ :=
     own rec_fn_name (to_fns fns).
@@ -474,6 +534,192 @@ Local Ltac unseal := rewrite
 Section lemmas.
   Context `{!recGS Σ}.
 
+  (** mapsto ghost state  *)
+  Lemma rec_mapsto_lookup h l v :
+    rec_mapsto_auth h -∗ l ↦ v -∗ ⌜h !! l = Some v⌝.
+  Proof. unseal. apply ghost_map_lookup. Qed.
+
+  Lemma rec_mapsto_update h l v v' :
+    rec_mapsto_auth h -∗ l ↦ v ==∗ rec_mapsto_auth (alter (λ _, v') l h) ∗ l ↦ v'.
+  Proof.
+    iIntros "Hh Hl".
+    iDestruct (rec_mapsto_lookup with "Hh Hl") as %?.
+    unseal.
+    iMod (ghost_map_update with "Hh Hl") as "[? $]".
+    have -> : (<[l:=v']> h) = (alter (λ _ : val, v') l h); [|done].
+    apply partial_alter_ext => ??. by simplify_map_eq.
+  Qed.
+
+  Lemma rec_mapsto_alloc_big h' h :
+    h' ##ₘ h →
+    rec_mapsto_auth h ==∗
+    rec_mapsto_auth (h' ∪ h) ∗ [∗ map] l↦v∈h', l ↦ v.
+  Proof. unseal. apply ghost_map_insert_big. Qed.
+
+  Local Transparent heap_alloc_h.
+  Lemma rec_mapsto_alloc h l sz :
+    heap_is_fresh h l →
+    rec_mapsto_auth (h_heap h) ==∗
+    rec_mapsto_auth (h_heap (heap_alloc h l sz)) ∗ [∗ list] o∈seqZ 0 sz, (l +ₗ o) ↦ 0.
+  Proof.
+    iIntros (Ha) "Hh".
+    iMod (rec_mapsto_alloc_big with "Hh") as "[$ ?]".
+    { apply map_disjoint_list_to_map_l. apply Forall_forall => -[??] /= /elem_of_list_fmap[?[??]].
+      simplify_eq. apply eq_None_not_Some => /heap_wf. unfold heap_is_fresh in *. naive_solver. }
+    rewrite big_sepM_list_to_map. 2: {
+      apply NoDup_alt => ???. do 2 setoid_rewrite list_lookup_fmap_Some.
+      setoid_rewrite lookup_seqZ => ??. naive_solver lia. }
+    by rewrite big_sepL_fmap.
+  Qed.
+  Local Opaque heap_alloc_h.
+
+  Lemma rec_mapsto_alloc_list h ls h' szs :
+    heap_alloc_list szs ls h h' →
+    rec_mapsto_auth (h_heap h) ==∗
+    rec_mapsto_auth (h_heap h') ∗ ([∗ list] l;n∈ls; szs, [∗ list] o∈seqZ 0 n, (l +ₗ o) ↦ 0).
+  Proof.
+    iIntros (Ha) "Hh".
+    iInduction (szs) as [|sz szs] "IH" forall (ls h h' Ha); destruct!/=. { by iFrame. }
+    iMod (rec_mapsto_alloc with "Hh") as "[Hh $]"; [done|].
+    iApply ("IH" with "[//] Hh").
+  Qed.
+
+  Lemma seqZ_succ m i :
+    0 ≤ i →
+    seqZ m (Z.succ i) = seqZ m i ++ [m + i].
+  Proof. intros ?. by rewrite -(Z2Nat.id i) // -Nat2Z.inj_succ seqZ_S. Qed.
+
+  Lemma rec_mapsto_free h l sz :
+    heap_range h l sz →
+    0 ≤ sz →
+    rec_mapsto_auth (h_heap h) -∗
+    ([∗ list] o∈seqZ 0 sz, ∃ v, (l +ₗ o) ↦ v) ==∗
+    rec_mapsto_auth (h_heap (heap_free h l)).
+  Proof.
+    iIntros (Hr Hsz) "Ha Hl".
+    iAssert (∃ vs, ⌜sz = length vs⌝ ∗ [∗ map] l↦v∈array l vs, l↦v)%I with "[Hl]" as (vs ?) "Hl".
+    { clear Hr.
+      iInduction sz as [|sz|sz] "IH" using (Z.succ_pred_induction 0) forall (Hsz).
+      { iExists []. iSplit!. } 2: { lia. }
+      rewrite seqZ_succ // big_sepL_app /=. iDestruct "Hl" as "[Hl [[%v Hv] _]]". rewrite Z.add_0_l.
+      iDestruct ("IH" with "[//] [$]") as (vs ?) "?". subst.
+      iExists (vs ++ [v]). iSplit; [iPureIntro; rewrite app_length /=; lia|].
+      rewrite array_app array_cons array_nil. iApply (big_sepM_union_2 with "[$]").
+      by iApply (big_sepM_insert_2 with "[Hv]").
+    }
+    unseal.
+    iMod (ghost_map_delete_big with "Ha [$]") => /=.
+    have -> : (h_heap h ∖ array l vs = (filter (λ '(l', _), l'.1 ≠ l.1) (h_heap h))); [|done].
+    apply map_eq => i. apply option_eq => v.
+    rewrite map_filter_lookup_Some lookup_difference_Some.
+    rewrite array_lookup_None.
+    unfold heap_range in Hr. split; [|naive_solver lia].
+    move => [Hh ?]. split!. move => ?. have := Hr i. rewrite Hh /is_Some. naive_solver lia.
+  Qed.
+
+  (** alloc ghost state  *)
+  Lemma rec_alloc_fake h :
+    rec_alloc_auth ∅ -∗ rec_alloc_auth h.
+  Proof.
+    unseal. iDestruct 1 as (m Hsz Hl) "?".
+    have -> : m = ∅. {
+      apply map_empty => i. apply eq_None_ne_Some_2 => ??.
+      have := Hl _ _ ltac:(done) (i, 0). set_unfold. naive_solver lia. }
+    iExists ∅. iFrame. iPureIntro. naive_solver.
+  Qed.
+
+  Local Transparent heap_alloc_h.
+  Lemma rec_alloc_alloc h l sz :
+    heap_is_fresh h l →
+    0 < sz →
+    rec_alloc_auth (dom (h_heap h)) ==∗
+    rec_alloc_auth (dom (h_heap (heap_alloc h l sz))) ∗ rec_alloc l sz.
+  Proof.
+    iIntros ([Hn Hl0] ?) "Ha". unseal.
+    iDestruct "Ha" as (m Hsz Hin) "Ha".
+    iMod (ghost_map_insert l.1 sz with "Ha") as "[Ha ?]". {
+      apply eq_None_not_Some => -[??].
+      have [//|_ Hdom]:= Hin _ _ ltac:(done) (l.1, 0).
+      apply Hn. apply (heap_wf _ (l.1, 0)). apply elem_of_dom. naive_solver lia.
+    }
+    iModIntro. iFrame. iSplit!; [..|done].
+    - move => ?? /lookup_insert_Some. naive_solver lia.
+    - move => ?? /lookup_insert_Some[[??]|[??]] l' ?; simplify_eq.
+      + rewrite dom_union_L elem_of_union dom_list_to_map_L.
+        set_unfold. setoid_rewrite elem_of_seq.
+        split; move => ?; destruct!/=.
+        * lia.
+        * revert select (_ ∈ _) => /elem_of_dom/heap_wf. congruence.
+        * left.  eexists (l +ₗ l'.2, _) => /=. split. { apply loc_eq. split!. lia. }
+          eexists _. split; [done|]. eexists (Z.to_nat l'.2). lia.
+      + rewrite dom_union_L elem_of_union dom_list_to_map_L.
+        split; move => ?; destruct!/=. 1: set_solver. all: naive_solver lia.
+  Qed.
+  Local Opaque heap_alloc_h.
+
+  Lemma rec_alloc_alloc_list szs h h' ls :
+    heap_alloc_list szs ls h h' →
+    rec_alloc_auth (dom (h_heap h)) ==∗
+    rec_alloc_auth (dom (h_heap h')) ∗ [∗ list] l;sz∈ls;szs, rec_alloc l sz.
+  Proof.
+    iIntros (Ha) "Ha".
+    iInduction (szs) as [|sz szs] "IH" forall (ls h h' Ha); destruct!/=. { by iFrame. }
+    iMod (rec_alloc_alloc with "Ha") as "[Ha $]"; [done|].
+    iApply ("IH" with "[//] Ha").
+  Qed.
+
+  Lemma rec_alloc_range h l sz :
+    rec_alloc_auth (dom (h_heap h)) -∗
+    rec_alloc l sz -∗
+    ⌜heap_range h l sz⌝.
+  Proof.
+    unseal. iDestruct 1 as (p Hsz Hl) "?". iIntros "[% ?]".
+    iDestruct (ghost_map_lookup with "[$] [$]") as %?.
+    iPureIntro. setoid_rewrite elem_of_dom in Hl.
+    move => ??. rewrite Hl //. lia.
+  Qed.
+
+  Lemma rec_alloc_size h l sz :
+    rec_alloc_auth (dom (h_heap h)) -∗
+    rec_alloc l sz -∗
+    ⌜0 < sz⌝.
+    unseal. iDestruct 1 as (p Hsz Hl) "?". iIntros "[% ?]".
+    iDestruct (ghost_map_lookup with "[$] [$]") as %?.
+    iPureIntro. naive_solver lia.
+  Qed.
+
+  Lemma rec_alloc_free h l sz :
+    rec_alloc_auth (dom (h_heap h)) -∗
+    rec_alloc l sz ==∗
+    rec_alloc_auth (dom (h_heap (heap_free h l))).
+  Proof.
+    unseal. iDestruct 1 as (p Hsz Hl) "?". iIntros "[% ?]".
+    iMod (ghost_map_delete with "[$] [$]"). iModIntro. iExists _. iFrame.
+    iPureIntro. split => ?? /lookup_delete_Some. 1: naive_solver.
+    move => [??] ?? /=. rewrite -Hl //.
+    rewrite !elem_of_dom map_filter_lookup_true //. naive_solver.
+  Qed.
+
+  Lemma rec_alloc_free_list h ls szs :
+    rec_mapsto_auth (h_heap h) -∗
+    rec_alloc_auth (dom (h_heap h)) -∗
+    ([∗ list] l;sz ∈ ls;szs, rec_alloc l sz) -∗
+    ([∗ list] l;n ∈ ls;szs, [∗ list] o ∈ seqZ 0 n, ∃ v0 : val, (l +ₗ o) ↦ v0) ==∗
+    ∃ h', ⌜heap_free_list (zip ls szs) h h'⌝ ∗ rec_mapsto_auth (h_heap h') ∗ rec_alloc_auth (dom (h_heap h')).
+  Proof.
+    iIntros "Hh Ha Has Hls".
+    iInduction (szs) as [|sz szs] "IH" forall (h ls).
+    { iModIntro. iDestruct (big_sepL2_nil_inv_r with "Has") as %?. simplify_eq/=. iSplit!. iFrame. }
+    iDestruct (big_sepL2_cons_inv_r with "Has") as (???) "[??]".
+    iDestruct (big_sepL2_cons_inv_r with "Hls") as (???) "[??]". simplify_eq/=.
+    iDestruct (rec_alloc_range with "[$] [$]") as %?.
+    iDestruct (rec_alloc_size with "[$] [$]") as %?.
+    iMod (rec_mapsto_free with "Hh [$]") as "Hh"; [done|lia|].
+    iMod (rec_alloc_free with "Ha [$]") as "Ha".
+    iMod ("IH" with "Hh Ha [$] [$]") as (??) "[??]". iModIntro. iSplit!; [done|]. iFrame.
+  Qed.
+
+  (** fn ghost state  *)
   Global Instance rec_fn_auth_pers fns : Persistent (rec_fn_auth fns).
   Proof. unseal. apply _. Qed.
 
@@ -663,29 +909,33 @@ Section lifting.
   Proof.
     iIntros "Hl HΦ".
     iApply sim_tgt_expr_step_None => /=. iIntros (? [e' h fns] ?? [??] Hp) "[Hh [Ha Hfns]]". simplify_eq/=.
+    iDestruct (rec_mapsto_lookup with "[$] [$]") as %?.
     exploit prim_step_inv_head; [done|..].
     { apply sub_redexes_are_values_item; case; naive_solver. }
     { done. }
     move => [? [Hstep ?]]. inv Hstep.
     iSpecialize ("HΦ" with "[$]").
-    do 2 iModIntro. iExists _, (_ @ -). iSplit!. admit.
+    do 2 iModIntro. iExists _, (_ @ -). iSplit!.
     iFrame. by iApply sim_tgt_expr_stop2.
-  Admitted.
+  Qed.
 
   Lemma sim_tgt_rec_Store Π Φ l v v' :
     l ↦ v -∗
-    (l ↦ v' -∗ ▷ₒ Φ (Val 0 @ -) Π) -∗
+    (l ↦ v' -∗ ▷ₒ Φ (Val v' @ -) Π) -∗
     TGT Store (Val (ValLoc l)) (Val v') @ - [{ Π }] {{ Φ }}.
   Proof.
     iIntros "Hl HΦ".
     iApply sim_tgt_expr_step_None => /=. iIntros (? [e' h fns] ?? [??] Hp) "[Hh [Ha Hfns]]". simplify_eq/=.
+    iDestruct (rec_mapsto_lookup with "[$] [$]") as %?.
+    iMod (rec_mapsto_update with "[$] [$]") as "[??]".
+    iSpecialize ("HΦ" with "[$]").
     exploit prim_step_inv_head; [done|..].
     { apply sub_redexes_are_values_item; case; naive_solver. }
     { done. }
     move => [? [Hstep ?]]. inv Hstep.
-    do 2 iModIntro. iExists _, (_ @ -). iSplit!. admit.
-    iFrame => /=.
-  Admitted.
+    do 2 iModIntro. iExists _, (_ @ -). iSplit!. { by eexists. }
+    iFrame => /=. rewrite dom_alter_L. iFrame. by iApply sim_tgt_expr_stop2.
+  Qed.
 
   Lemma sim_tgt_rec_AllocA e Π Φ vs :
     Forall (λ z, 0 < z) vs.*2 →
@@ -700,8 +950,23 @@ Section lifting.
     { apply sub_redexes_are_values_item; case; naive_solver. }
     { done. }
     move => [? [Hstep ?]]. inv Hstep.
-    do 2 iModIntro. iExists _, (_ @ -). iSplit!. iFrame. simpl.
-  Admitted.
+    iMod (rec_mapsto_alloc_list with "Hh") as "[Hh ?]"; [done|].
+    iMod (rec_alloc_alloc_list with "Ha") as "[Ha Has]"; [done|].
+    iSpecialize ("HΦ" with "[$]").
+    do 2 iModIntro. iExists _, (_ @ -). iSplit!. iFrame => /=.
+    iApply (sim_tgt_expr_bind [FreeACtx _] (_ @ -) with "[-]") => /=.
+    iApply (sim_tgt_expr_wand with "HΦ") => /=.
+    iIntros (? ?) "[% [% [Hl HΦ]]]" => /=. simplify_eq/=.
+
+    iApply sim_tgt_expr_step_None => /=. iIntros (? [e' h'' fns'] ?? [??] Hp') "[Hh [Ha Hfns]]". simplify_eq/=.
+    exploit prim_step_inv_head; [done|..].
+    { apply sub_redexes_are_values_item; case; naive_solver. }
+    { done. }
+    move => [? [Hstep ?]]. inv Hstep.
+    iMod (rec_alloc_free_list with "Hh Ha [$] [$]") as (??) "[??]".
+    do 2 iModIntro. iExists _, (_ @ -). iSplit!; [done|]. iFrame => /=.
+    by iApply sim_tgt_expr_stop2.
+  Qed.
 
   Lemma sim_tgt_rec_LetE Π Φ s v e :
     (▷ₒ TGT (subst s v e) @ - [{ Π }] {{ Φ }}) -∗
@@ -733,67 +998,6 @@ End lifting.
 
 Section memmove.
   Context `{!dimsumGS Σ} `{!recGS Σ}.
-
-
-  Local Definition array (l : loc) (vs : list val) : gmap loc val :=
-    (kmap (λ i, l +ₗ i) (map_seqZ 0 vs)).
-
-  Lemma loc_eq (l1 l2 : loc) :
-    l1 = l2 ↔ l1.1 = l2.1 ∧ l1.2 = l2.2.
-  Proof. destruct l1, l2; naive_solver. Qed.
-
-  Lemma array_nil l :
-    array l [] = ∅.
-  Proof. rewrite /array/=. apply kmap_empty. Qed.
-  Lemma array_cons l v vs :
-    array l (v::vs) = <[l:=v]> $ array (l +ₗ 1) vs.
-  Proof.
-    rewrite /array/= kmap_insert offset_loc_0. f_equal.
-    apply map_eq => ?. apply option_eq => ?.
-    rewrite !lookup_kmap_Some. setoid_rewrite lookup_map_seqZ_Some.
-    split => -[i [? [? <-]]]; simplify_eq.
-    - eexists (Z.pred i). split!; [|lia|f_equal; lia].
-      unfold offset_loc. destruct l => /=. f_equal. lia.
-    - eexists (Z.succ i). split!; [|lia|f_equal; lia].
-      unfold offset_loc. destruct l => /=. f_equal. lia.
-  Qed.
-
-  Lemma array_app l vs1 vs2 :
-    array l (vs1 ++ vs2) = array l vs1 ∪ array (l +ₗ length vs1) vs2.
-  Proof.
-    elim: vs1 l. { move => ?/=. by rewrite array_nil offset_loc_0 left_id_L. }
-    move => v vs1 IH l /=. rewrite !array_cons -insert_union_l.
-    rewrite IH. do 3 f_equal. apply loc_eq. split!. lia.
-  Qed.
-
-  Lemma array_insert l1 l2 v vs :
-    l1.1 = l2.1 →
-    l2.2 ≤ l1.2 < l2.2 + length vs →
-    <[l1:=v]> $ array l2 vs = array l2 (<[Z.to_nat (l1.2 - l2.2):=v]>vs).
-  Proof.
-    move => ??. have {1} -> : l1 = l2 +ₗ Z.to_nat (l1.2 - l2.2).
-    { unfold offset_loc. destruct l1, l2; simplify_eq/=. f_equal. lia. }
-    rewrite /array/= map_seqZ_insert. 2: lia.
-    by rewrite kmap_insert.
-  Qed.
-
-  Lemma array_lookup_None l l' vs :
-    array l vs !! l' = None ↔ (l.1 = l'.1 → l'.2 < l.2 ∨ l.2 + length vs ≤ l'.2).
-  Proof.
-    rewrite /array.
-    rewrite lookup_kmap_None.
-    setoid_rewrite lookup_map_seqZ_None.
-    split.
-    - move => Hi Heq.
-      exploit (Hi (l'.2 - l.2)). { unfold offset_loc. destruct l, l'; simplify_eq/=. f_equal. lia. }
-      lia.
-    - move => Hi ??. simplify_eq/=. lia.
-  Qed.
-
-  Lemma array_lookup_is_Some l l' vs :
-    is_Some (array l vs !! l') ↔ l.1 = l'.1 ∧ l.2 ≤ l'.2 < l.2 + length vs.
-  Proof. rewrite -not_eq_None_Some array_lookup_None. naive_solver lia. Qed.
-
 
   Lemma sim_memcpy Π Φ d d' s' s n o hvss hvsd :
     n = Z.of_nat (length hvss) →
@@ -1057,7 +1261,10 @@ Section memmove.
     iApply sim_src_stop. iSplit!. iApply bi_mono1_intro0. iIntros (?) "Htgt".
     iApply (sim_tgt_handler_intro with "[-]").
     iApply (sim_tgt_link_left with "[-]").
-    iApply ("Htgt" with "[Hh] [Ha]"). admit. admit.
+    iApply fupd_sim_tgt.
+    iMod (rec_mapsto_alloc_big (h_heap h) with "Hh") as "[Hh _]". { apply map_disjoint_empty_r. }
+    iApply ("Htgt" with "[Hh] [Ha]"). { by rewrite right_id_L. }
+    { rewrite dom_empty_L. by iApply rec_alloc_fake. }
     iApply (sim_tgt_expr_bind [ReturnExtCtx _] (_ @ -)).
     iApply sim_tgt_rec_Call_internal. 2: { by iApply (rec_fn_intro with "[$]"). } { done. }
     iModIntro => /=.
@@ -1179,7 +1386,7 @@ Section memmove.
       iIntros (??????). destruct!/=.
       iApply "Hsrc". iApply sim_src_TUb_end.
     Unshelve. exact: tt.
-  Admitted.
+  Qed.
 End memmove.
 
 Lemma memmove_refines_spec :
