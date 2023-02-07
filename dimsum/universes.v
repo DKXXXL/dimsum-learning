d91f9932c6e6fd3d113924e826f973e39bdf97fa
Michael Sammler <noreply@sammler.me>
2023-02-07 14:45:12 +0100

    add BelowState universe

--- dimsum/universes.v
+++ dimsum/universes.v
@@ -2,6 +2,8 @@ From iris.algebra Require Import ofe.
 From dimsum Require Import base.
 
 Module Import universes.
+  (** A universe below the state of modules. *)
+  Universe BelowState.
   (** The universe of the state of modules. *)
   Universe State.
   (** The universe of events. *)
@@ -10,10 +12,12 @@ Module Import universes.
   Universe Ordinal.
 End universes.
 
+Notation TypeBelowState := Type@{BelowState} (only parsing).
 Notation TypeState := Type@{State} (only parsing).
 Notation TypeEvent := Type@{Event} (only parsing).
 Notation TypeOrdinal := Type@{Ordinal} (only parsing).
 Notation TypeOfe := Type@{ofe.u0} (only parsing).
 
+Constraint BelowState < State.
 Constraint State < Ordinal.
 Constraint Ordinal <= ofe.u0.
