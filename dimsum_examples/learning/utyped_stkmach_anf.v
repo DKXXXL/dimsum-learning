a22d6b732aea3f09e4ea0bfe30d50c4d85b0d820
Ende Jin <ende.jin@uwaterloo.ca>
2023-06-25 16:46:41 -0400

    Save.

--- dimsum_examples/learning/utyped_stkmach_anf.v
+++ dimsum_examples/learning/utyped_stkmach_anf.v
@@ -52,6 +52,13 @@ Inductive step : stk -> stkOp -> stk -> Prop :=
 
 Definition cfg : Set := stk * stkOps.
 
+Inductive step' : stk -> stkOps -> stk -> stkOps -> Prop :=
+  | st_ :
+    forall x stk0 ops0 stk1 ops1,
+      ops0 = x :: ops1 ->
+      step stk0 x stk1 ->
+      step' stk0 ops0 stk1 ops1.
+
 
 End STKM.
 
@@ -67,9 +74,9 @@ Inductive EVal : Set :=
   | Const : val -> EVal. 
 
 Inductive EComp : Set := 
-| Lets : ID -> EComp -> EComp -> EComp 
-| BinOp : binop -> EVal -> EVal -> EComp
-| rs : EVal -> EComp.
+  | Lets : ID -> EComp -> EComp -> EComp 
+  | BinOp : binop -> EVal -> EVal -> EComp
+  | rs : EVal -> EComp.
 
 (* | alloc : forall {G T}, expr G T -> expr G (tyref T)
 | assign : forall {G T}, expr G (tyref T) -> expr G T -> expr G T 
@@ -88,6 +95,8 @@ Inductive ECont : Set :=
   | CLet : ID -> ECont -> EComp -> ECont
   | CLetPop : env -> ECont -> ECont.
 
+Definition cfg : Set := env * EComp * ECont.
+
 Inductive step : env -> EComp -> ECont ->
                  env -> EComp -> ECont -> Prop :=
   | st_lets : forall E x binding body K,
@@ -105,5 +114,75 @@ Inductive step : env -> EComp -> ECont ->
 End LET.
 
 
-(* Now we start compilation *)
+(* Now we start compilation, intensional way of proving compilation correctness *)
+
+(* Proof relevant matching -- we have a function  *)
+(* Definition match_env : LET.env -> STKM.stk -> Type := 
+  {g : ID -> option nat |
+    forall i, 
+
+  
+  }. *)
+
+Axiom match_env : LET.env -> STKM.stk -> Set.
+
+
+Section FixEnvStk.
+
+Context {env0 : LET.env} .
+Context {stk0 : STKM.stk}.
+Variable matched_es : match_env env0 stk0.
+
+
+(* given matched env0 and stk0 *)
+Definition compile : LET.EComp -> STKM.stkOps.
+Admitted.
+
+(* Axiom compile_cont : LET.ECont -> STKM.stkOps.  *)
+
+(* Intention 1 was *)
+(* env0 ⊢ LET.ECont *)
+(* stk0 ⊢ STKM.stkOps *)
+Inductive match_cont : LET.ECont -> STKM.stkOps -> Prop :=
+  | mc_end :  match_cont LET.END []
+  | mc_CLet :
+    forall kont code x body,
+      match_cont kont code ->
+      (* but here it breaks the intention, 
+         because body should well typed in env0[x -> ...] *)
+      match_cont (LET.CLet x kont body) ((compile body) ++ code)
+  | mc_CLetPop :
+    forall kont code,
+      match_cont kont code ->
+      match_cont (LET.CLetPop env0 kont) code.
+
+End FixEnvStk.
+
+(* Print compile. *)
+
+Definition match_config  :
+  LET.env -> LET.EComp -> LET.ECont ->  STKM.stk -> STKM.stkOps -> Prop :=
+  fun ev comp kont stk code =>
+    exists (g : (match_env ev stk)), 
+    (exists compcode kontcode,
+      match_cont g kont kontcode /\ 
+      compile g comp = compcode /\ 
+      code = compcode ++ kontcode).
+      
+
+Print existT.
+(* back simulation *)
+Lemma match_config_inv :
+  forall stk op stk' ,
+    STKM.step stk op stk' ->
+    forall ev comp kont x code code',
+    code = x :: code' ->
+    match_config ev comp kont stk code ->
+    exists ev' comp' kont',
+    (LET.step ev  comp  kont 
+             ev' comp' kont') /\ 
+    (match_config ev' comp' kont' stk' code').
+intros stk op stk' h.
+induction h; intros; subst; eauto;
+unfold match_config in *.
 
